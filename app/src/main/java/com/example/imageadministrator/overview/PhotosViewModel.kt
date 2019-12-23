674afc992fc945fe37ed37325673b33e18914ce4
RR <Roberto_Rubio98@outlook.com>
2019-12-23 17:11:35 -0600

    Added PhotosViewModel :sparkles:

---
+++ app/src/main/java/com/example/imageadministrator/overview/PhotosViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.imageadministrator.overview
+
+import androidx.lifecycle.ViewModel
+import com.example.imageadministrator.network.PhotosModel
+
+class PhotosViewModel(var photoItem : PhotosModel) : ViewModel()
