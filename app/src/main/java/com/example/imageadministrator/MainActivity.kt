b7c5ed95a4ae93aebeab12b7ee9389aa48941368
RR <Roberto_Rubio98@outlook.com>
2019-12-23 17:07:00 -0600

    Added showData function :sparkles:

--- app/src/main/java/com/example/imageadministrator/MainActivity.kt
+++ app/src/main/java/com/example/imageadministrator/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.example.imageadministrator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.example.imageadministrator.databinding.ActivityMainBinding
+import com.example.imageadministrator.overview.MainViewModel
+
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+    lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        binding.viewModel = viewModel
+        showData()
     }
+
+    fun showData(){
+        binding.viewModel?.getListPhoto()?.observe(this, Observer {
+            Log.v("aaaaa", "${it.size}")
+        })
+
+    }
+
 }
