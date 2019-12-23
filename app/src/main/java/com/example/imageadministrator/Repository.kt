f080a63e6beb8ef9c85bb344f6f0080808e5467e
RR <Roberto_Rubio98@outlook.com>
2019-12-23 17:12:12 -0600

    Added Repository:sparkles:

---
+++ app/src/main/java/com/example/imageadministrator/Repository.kt
@@ -0,0 +1,25 @@
+package com.example.imageadministrator
+
+import androidx.lifecycle.MutableLiveData
+import com.example.imageadministrator.network.PhotosApi
+import com.example.imageadministrator.network.PhotosModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class Repository {
+    private val client  = PhotosApi.getRetrofitInstance()
+
+    fun getPhotoData() : MutableLiveData<List<PhotosModel>> {
+        val liveData = MutableLiveData<List<PhotosModel>>()
+        client.getPhotos().enqueue(object : Callback<List<PhotosModel>> {
+            override fun onResponse(call: Call<List<PhotosModel>>, response: Response<List<PhotosModel>>) {
+
+                liveData.value = response.body()
+            }
+            override fun onFailure(call: Call<List<PhotosModel>>, t: Throwable) {
+            }
+        })
+        return  liveData
+    }
+}
