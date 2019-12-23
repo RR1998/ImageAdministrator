a81fa9ada54dff9ee64fdbab67230a4c94459d70
RR <Roberto_Rubio98@outlook.com>
2019-12-23 17:09:35 -0600

    PhotosApi :sparkles:

---
+++ app/src/main/java/com/example/imageadministrator/network/PhotosApi.kt
@@ -0,0 +1,20 @@
+package com.example.imageadministrator.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+object PhotosApi {
+
+    lateinit var retrofit: Retrofit
+
+    private const val imageUrl: String = "https://jsonplaceholder.typicode.com/"
+
+    fun getRetrofitInstance(): GetDataService {
+        retrofit = Retrofit.Builder()
+            .baseUrl(imageUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        return retrofit.create(GetDataService::class.java)
+    }
+}
