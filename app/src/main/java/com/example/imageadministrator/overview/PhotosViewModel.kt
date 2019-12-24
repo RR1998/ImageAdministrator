9b7a629c573c4b69dcebcb928b4cf40da43c3149
RR <Roberto_Rubio98@outlook.com>
2019-12-24 12:51:19 -0600

    Added onClick function to PhotosViewModel :sparkles:

--- app/src/main/java/com/example/imageadministrator/overview/PhotosViewModel.kt
+++ app/src/main/java/com/example/imageadministrator/overview/PhotosViewModel.kt
@@ -1,6 +1,23 @@
 package com.example.imageadministrator.overview
 
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.core.content.ContextCompat.startActivity
 import androidx.lifecycle.ViewModel
+import com.example.imageadministrator.DetailActivity
 import com.example.imageadministrator.network.PhotosModel
 
-class PhotosViewModel(var photoItem : PhotosModel) : ViewModel()
+class PhotosViewModel(var photoItem : PhotosModel) : ViewModel(){
+    //TODO remove context as parameter
+    fun onClick(context: Context){
+        val detailClassIntent = Intent(
+            context,
+            DetailActivity::class.java
+        )
+        val bundle = Bundle()
+        bundle.putString("imageUrl", photoItem.url)
+        detailClassIntent.putExtras(bundle)
+        startActivity(context, detailClassIntent, null)
+    }
+}
