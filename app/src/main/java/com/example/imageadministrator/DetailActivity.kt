d0ee7391ac86486d0103899ea8aaa4e62b0a5df6
RR <Roberto_Rubio98@outlook.com>
2019-12-24 12:50:36 -0600

    Added DetailActivity :sparkles:

---
+++ app/src/main/java/com/example/imageadministrator/DetailActivity.kt
@@ -0,0 +1,21 @@
+package com.example.imageadministrator
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
+import com.example.imageadministrator.databinding.ActivityMainBinding
+import com.example.imageadministrator.overview.DetailViewModel
+
+class DetailActivity: AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var viewModel: DetailViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.detail_layout)
+        viewModel = ViewModelProviders.of(this).get(DetailViewModel::class.java)
+
+    }
+}
