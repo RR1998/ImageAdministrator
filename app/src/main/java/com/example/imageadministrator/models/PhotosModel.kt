53b5b72ae95657a0eb3b23907d6584d54415bef8
RR <Roberto_Rubio98@outlook.com>
2019-12-24 18:03:23 -0600

    Implemented parcelable :sparkles:

--- app/src/main/java/com/example/imageadministrator/models/PhotosModel.kt
+++ app/src/main/java/com/example/imageadministrator/models/PhotosModel.kt
@@ -1,9 +1,42 @@
 package com.example.imageadministrator.models
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class PhotosModel(
     val albumId: Int,
     val id: Int,
-    val title: String,
-    val url: String,
-    val thumbnailUrl: String
-)
+    val title: String?,
+    val url: String?,
+    val thumbnailUrl: String?
+): Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readInt(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString()
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(albumId)
+        parcel.writeInt(id)
+        parcel.writeString(title)
+        parcel.writeString(url)
+        parcel.writeString(thumbnailUrl)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<PhotosModel> {
+        override fun createFromParcel(parcel: Parcel): PhotosModel {
+            return PhotosModel(parcel)
+        }
+
+        override fun newArray(size: Int): Array<PhotosModel?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
