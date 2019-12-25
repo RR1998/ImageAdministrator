7a54a0121ec2d18cdb3f0e64de41480b9de77ad5
RR <Roberto_Rubio98@outlook.com>
2019-12-24 18:02:40 -0600

    Changed onClick function :zap:

--- app/src/main/java/com/example/imageadministrator/viewmodels/PhotosViewModel.kt
+++ app/src/main/java/com/example/imageadministrator/viewmodels/PhotosViewModel.kt
@@ -1,23 +1,11 @@
 package com.example.imageadministrator.viewmodels
 
-import android.content.Context
-import android.content.Intent
-import android.os.Bundle
-import androidx.core.content.ContextCompat.startActivity
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.imageadministrator.activities.DetailActivity
 import com.example.imageadministrator.models.PhotosModel
 
-class PhotosViewModel(var photoItem : PhotosModel) : ViewModel(){
-    //TODO remove context as parameter
-    fun onClick(context: Context){
-        val detailClassIntent = Intent(
-            context,
-            DetailActivity::class.java
-        )
-        val bundle = Bundle()
-        bundle.putString("imageUrl", photoItem.url)
-        detailClassIntent.putExtras(bundle)
-        startActivity(context, detailClassIntent, null)
+class PhotosViewModel(var photoItem : PhotosModel, var clickEvent: MutableLiveData<PhotosModel>) : ViewModel(){
+    fun onClick(){
+        clickEvent.value = photoItem
     }
 }
