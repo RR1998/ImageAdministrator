1c75abdb13f9650c727d408dd7035be22fa06b88
RR <Roberto_Rubio98@outlook.com>
2019-12-24 18:05:11 -0600

    Added clickObserver function :sparkles:

--- app/src/main/java/com/example/imageadministrator/activities/MainActivity.kt
+++ app/src/main/java/com/example/imageadministrator/activities/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.imageadministrator.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -9,6 +11,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.imageadministrator.R
 import com.example.imageadministrator.adapter.ImageAdapter
 import com.example.imageadministrator.databinding.ActivityMainBinding
+import com.example.imageadministrator.models.PhotosModel
 import com.example.imageadministrator.viewmodels.MainViewModel
 
 
@@ -23,17 +26,25 @@ class MainActivity : AppCompatActivity() {
             R.layout.activity_main
         )
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        binding.viewModel = viewModel
-        showData()
+        binding.viewModel =  viewModel
+        viewModel.getListPhoto().observe(this, photosListObserver())
+        viewModel.itemClickEvent.observe(this, clickObserver())
     }
 
-    private fun showData(){
-        binding.viewModel?.getListPhoto()?.observe(this, Observer {
-            val adapter = ImageAdapter(it)
-            binding.photoViews.layoutManager = LinearLayoutManager(this)
-            binding.photoViews.adapter = adapter
-        })
-
+    private fun clickObserver() = Observer<PhotosModel> {
+        val detailClassIntent = Intent(
+            this,
+            DetailActivity::class.java
+        )
+        val bundle = Bundle()
+        bundle.putParcelable("imageModel", it)
+        detailClassIntent.putExtras(bundle)
+        ContextCompat.startActivity(this, detailClassIntent, null)
     }
 
+    private fun photosListObserver() = Observer<List<PhotosModel>> {
+        val adapter = ImageAdapter(it, viewModel.itemClickEvent)
+        binding.photoViews.layoutManager = LinearLayoutManager(this)
+        binding.photoViews.adapter = adapter
+    }
 }
