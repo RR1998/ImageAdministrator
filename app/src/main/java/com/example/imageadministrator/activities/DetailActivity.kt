611d712e2c85e347822b6defe995d4470c9be06f
RR <Roberto_Rubio98@outlook.com>
2019-12-24 18:04:42 -0600

    Added setImageUrl function :sparkles:

--- app/src/main/java/com/example/imageadministrator/activities/DetailActivity.kt
+++ app/src/main/java/com/example/imageadministrator/activities/DetailActivity.kt
@@ -1,24 +1,36 @@
 package com.example.imageadministrator.activities
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
+import com.bumptech.glide.Glide
 import com.example.imageadministrator.R
 import com.example.imageadministrator.databinding.DetailLayoutBinding
+import com.example.imageadministrator.models.PhotosModel
 import com.example.imageadministrator.viewmodels.DetailViewModel
 
 class DetailActivity: AppCompatActivity() {
 
     lateinit var binding: DetailLayoutBinding
-    lateinit var viewModel: DetailViewModel
-
+    lateinit var detailViewModel: DetailViewModel
+    private var photos:PhotosModel? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val bundleReceived = intent.extras
+        bundleReceived?.getParcelable<PhotosModel>("imageModel").let {  photos = it}
+
         binding = DataBindingUtil.setContentView(this,
             R.layout.detail_layout
         )
-        viewModel = ViewModelProviders.of(this).get(DetailViewModel::class.java)
-
+        detailViewModel = ViewModelProviders.of(this).get(DetailViewModel::class.java)
+        binding.detailViewModel = detailViewModel
+        setImageUrl(binding.detailedImage, photos)
+    }
+    private fun setImageUrl(view: ImageView, detailModel: PhotosModel?) {
+        Glide.with(view.context).load(detailModel?.url)
+            .into(view)
     }
 }
+
