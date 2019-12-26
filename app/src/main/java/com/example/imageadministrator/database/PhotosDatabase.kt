7d93815f9da78938ec2a0cf98cd6c17aa6ceb431
RR <Roberto_Rubio98@outlook.com>
2019-12-26 10:10:26 -0600

    Added PhotosDatabase :sparkles:

---
+++ app/src/main/java/com/example/imageadministrator/database/PhotosDatabase.kt
@@ -0,0 +1,35 @@
+package com.example.imageadministrator.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.imageadministrator.models.PhotosModel
+
+@Database(entities = [PhotosModel::class], version = 1, exportSchema = false)
+abstract class PhotosDatabase(): RoomDatabase() {
+
+    abstract val photosDao: PhotosDataBaseDao
+
+    companion object{
+
+        @Volatile
+        private var INSTANCE: PhotosDatabase? = null
+
+        fun getInstance(context: Context):PhotosDatabase{
+            synchronized(this){
+                var instance = INSTANCE
+
+                if(instance == null){
+                    instance = Room.databaseBuilder(context.applicationContext,
+                                                    PhotosDatabase::class.java,
+                                              "photos_databse")
+                                                    .fallbackToDestructiveMigration().build()
+                    INSTANCE = instance
+                }
+
+                return instance
+            }
+        }
+    }
+}
