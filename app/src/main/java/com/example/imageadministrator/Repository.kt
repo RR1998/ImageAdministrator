eab5c1b4c965fb61ea505666fe04024c88309d30
RR <Roberto_Rubio98@outlook.com>
2019-12-26 17:00:07 -0600

    Added only twenty five image showed :sparkles:

--- app/src/main/java/com/example/imageadministrator/Repository.kt
+++ app/src/main/java/com/example/imageadministrator/Repository.kt
@@ -1,31 +1,38 @@
 package com.example.imageadministrator
 
 import androidx.lifecycle.MutableLiveData
-import com.example.imageadministrator.network.PhotosApi
+import com.example.imageadministrator.database.PhotosDatabase
 import com.example.imageadministrator.models.PhotosModel
+import com.example.imageadministrator.network.PhotosApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class Repository {
+class Repository(private val database: PhotosDatabase?) {
 
-    private val client  = PhotosApi.getRetrofitInstance()
+    private val client = PhotosApi.getRetrofitInstance()
 
-    fun getPhotoData() : MutableLiveData<List<PhotosModel>> {
+    val liveData = MutableLiveData<List<PhotosModel>>()
 
-        val liveData = MutableLiveData<List<PhotosModel>>()
+    fun getPhotoData(): MutableLiveData<List<PhotosModel>> {
+
 
         client.getPhotos().enqueue(object : Callback<List<PhotosModel>> {
 
-            override fun onResponse(call: Call<List<PhotosModel>>, response: Response<List<PhotosModel>>) {
-
+            override fun onResponse(
+                call: Call<List<PhotosModel>>,
+                response: Response<List<PhotosModel>>
+            ) {
                 liveData.value = response.body()
+                liveData.value = liveData.value?.subList(0, 24)
             }
 
             override fun onFailure(call: Call<List<PhotosModel>>, t: Throwable) {}
 
         })
 
-        return  liveData
+        return liveData
     }
 }
