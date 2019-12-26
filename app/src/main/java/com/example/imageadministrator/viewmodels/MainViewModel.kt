5e50004204a1262e3b87ee2671c8823618a1cd86
RR <Roberto_Rubio98@outlook.com>
2019-12-26 17:03:18 -0600

    Added launch insert coroutine to the repository

--- app/src/main/java/com/example/imageadministrator/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/example/imageadministrator/viewmodels/MainViewModel.kt
@@ -3,14 +3,38 @@ package com.example.imageadministrator.viewmodels
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.imageadministrator.Repository
+import com.example.imageadministrator.database.PhotosDatabase
 import com.example.imageadministrator.models.PhotosModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
-class MainViewModel : ViewModel() {
+class MainViewModel(private val database: PhotosDatabase?) : ViewModel() {
 
-    var itemClickEvent =  MutableLiveData<PhotosModel>()
-    var dataList : Repository = Repository()
-    fun getListPhoto(): MutableLiveData<List<PhotosModel>> {
-        return dataList.getPhotoData()
+    @Suppress("zero parameters constructor error")
+    constructor() : this(
+        database = null
+    )
+
+    var itemClickEvent: MutableLiveData<PhotosModel> = MutableLiveData()
+
+    private var dataListRepository: Repository = Repository(database)
+
+    private var mainViewModelJob = Job()
+
+    private var uiScope = CoroutineScope(Dispatchers.Main + mainViewModelJob)
+
+    override fun onCleared() {
+        super.onCleared()
+        mainViewModelJob.cancel()
     }
 
+    fun getListPhoto(): MutableLiveData<List<PhotosModel>> {
+        val dataList = dataListRepository.getPhotoData()
+        uiScope.launch {
+            dataListRepository.insertPhotos()
+        }
+        return dataList
+    }
 }
