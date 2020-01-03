555a877d48fb1e2b117a42e5697b2a3ee4332eb2
RR <Roberto_Rubio98@outlook.com>
2020-01-03 09:11:04 -0600

    Refactored code :recycle:

--- core/src/main/java/com/example/core/domain/PhotosCleanMapper.kt
+++ core/src/main/java/com/example/core/domain/PhotosCleanMapper.kt
@@ -1,7 +1,5 @@
 package com.example.core.domain
 
-import com.example.core.domain.PhotosCleanModel
-
 interface PhotosCleanMapper{
     fun cleaner(id: Int, albumId: Int, title: String, url: String, thumbnailUrl: String):PhotosCleanModel
 }
