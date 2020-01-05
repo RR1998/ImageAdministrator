b623cb5fd09010f1533d35213f043e0d20fb7e7f
RR <Roberto_Rubio98@outlook.com>
2020-01-05 17:39:57 -0600

    Added corrutine call :sparkles:

--- data/src/main/java/com/example/framework/datasource/ModelSourceImplementation.kt
+++ data/src/main/java/com/example/framework/datasource/ModelSourceImplementation.kt
@@ -3,15 +3,18 @@ package com.example.framework.datasource
 import androidx.lifecycle.MutableLiveData
 import com.example.core.domain.PhotosCleanModel
 import com.example.core.repository.PhotosDataSource
-import com.example.framework.database.PhotosGetDatabase
-import com.example.framework.models.PhotosEntityModel
+import com.example.framework.database.databaseapplication.DatabaseApplication
 import com.example.framework.models.PhotosResponseModel
 import com.example.framework.remote.PhotosApi
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class ModelSourceImplementation(private val database: PhotosGetDatabase?) : PhotosDataSource {
+//private val database: PhotosGetDatabase?
+class ModelSourceImplementation : PhotosDataSource {
+
+    var databaseApplicationInstance = DatabaseApplication()
 
     override fun getPhotoData(): MutableLiveData<List<PhotosCleanModel>> {
 
@@ -27,16 +30,21 @@ class ModelSourceImplementation(private val database: PhotosGetDatabase?) : Phot
                 call: Call<List<PhotosResponseModel>>,
                 response: Response<List<PhotosResponseModel>>
             ) {
+                databaseApplicationInstance.onCreate()
                 liveData.value = response.body()
                 liveData.value = liveData.value?.subList(0, 24)
                 liveData.value?.forEach {
                     cleanList.add(it.responseEntity().cleaner())
-                    database?.daoInterface()?.insert(it.responseEntity())
                 }
-                val listItems: MutableList<PhotosEntityModel> = mutableListOf()
-                database?.daoInterface()?.getPhotos()?.forEach {
+
+                databaseApplicationInstance.uiScope.launch {
+                    databaseApplicationInstance.savePhotos(response.body())
+                }
+                /*val listItems: MutableList<PhotosEntityModel> = mutableListOf()
+                databaseApplicationInstance.database.daoInterface().getPhotos().forEach {
                     listItems.add(it)
-                }
+                }*/
+
                 liveDataCleanList.value = cleanList
             }
 
