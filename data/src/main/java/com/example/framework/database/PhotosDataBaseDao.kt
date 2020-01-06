7a766d0895afb73e1d70f4e88047c03db34c3fc9
RR <Roberto_Rubio98@outlook.com>
2020-01-06 12:28:01 -0600

    Added onConflict to insert query :sparkles:

--- data/src/main/java/com/example/framework/database/PhotosDataBaseDao.kt
+++ data/src/main/java/com/example/framework/database/PhotosDataBaseDao.kt
@@ -2,13 +2,14 @@ package com.example.framework.database
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.framework.models.PhotosEntityModel
 
 @Dao
 interface PhotosDataBaseDao {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     fun insert(photo: PhotosEntityModel)
 
     @Query("SELECT * FROM photos LIMIT 25")
