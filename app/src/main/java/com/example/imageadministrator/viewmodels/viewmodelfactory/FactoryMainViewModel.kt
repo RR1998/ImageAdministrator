d1924b564076fe2dd44d67ccdf200b77bb85951e
RR <Roberto_Rubio98@outlook.com>
2020-01-06 12:10:57 -0600

    Added FactoryMainViewModel :sparkles:

---
+++ app/src/main/java/com/example/imageadministrator/viewmodels/viewmodelfactory/FactoryMainViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.imageadministrator.viewmodels.viewmodelfactory
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.core.interactors.GetPhotos
+import com.example.imageadministrator.viewmodels.MainViewModel
+
+@Suppress("UNCHECKED_CAST")
+class FactoryMainViewModel(private val useCase: GetPhotos): ViewModelProvider.Factory{
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return MainViewModel(useCase) as T
+    }
+
+}
