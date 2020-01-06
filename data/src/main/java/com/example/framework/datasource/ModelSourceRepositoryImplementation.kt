42c8d1f7be9f456d3ddd659be9e21f00e6ae022f
RR <Roberto_Rubio98@outlook.com>
2020-01-06 12:29:14 -0600

    Added savePhotos suspend function

---
+++ data/src/main/java/com/example/framework/datasource/ModelSourceRepositoryImplementation.kt
@@ -0,0 +1,58 @@
+package com.example.framework.datasource
+
+import androidx.lifecycle.MutableLiveData
+import com.example.core.domain.PhotosCleanModel
+import com.example.core.repository.PhotosDataSourceRepository
+import com.example.framework.database.PhotosGetDatabase
+import com.example.framework.models.PhotosResponseModel
+import com.example.framework.remote.PhotosApi
+import kotlinx.coroutines.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class ModelSourceRepositoryImplementation(val client: PhotosApi, var database: PhotosGetDatabase) :
+    PhotosDataSourceRepository {
+
+    private var insertDatabaseJob = Job()
+
+    var uiScope = CoroutineScope(Dispatchers.Main + insertDatabaseJob)
+
+    override fun getPhotoData(): MutableLiveData<List<PhotosCleanModel>> {
+        val liveDataCleanList = MutableLiveData<List<PhotosCleanModel>>()
+        val liveData = MutableLiveData<List<PhotosResponseModel>>()
+        val clientInstance = client.getRetrofitInstance()
+        clientInstance.getPhotos().enqueue(object : Callback<List<PhotosResponseModel>> {
+
+            val cleanList = mutableListOf<PhotosCleanModel>()
+
+            override fun onResponse(
+                call: Call<List<PhotosResponseModel>>,
+                response: Response<List<PhotosResponseModel>>
+            ) {
+                liveData.value = response.body()
+                liveData.value = liveData.value?.subList(0, 24)
+                liveData.value?.forEach {
+                    cleanList.add(it.responseToEntity().cleaner())
+                }
+
+                uiScope.launch {
+                    savePhotos(response.body())
+                }
+
+                liveDataCleanList.value = cleanList
+            }
+
+            override fun onFailure(call: Call<List<PhotosResponseModel>>, t: Throwable) {}
+        })
+        return liveDataCleanList
+    }
+
+    suspend fun savePhotos(cleanList: List<PhotosResponseModel>?) {
+        withContext(Dispatchers.IO) {
+            cleanList?.forEach {
+                database.daoInterface().insert(it.responseToEntity())
+            }
+        }
+    }
+}
