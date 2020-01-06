34f7e598e53f7d76368b8b6a196e23352dd7d3aa
RR <Roberto_Rubio98@outlook.com>
2020-01-06 12:28:46 -0600

    Deleted GetDatabaseInterface implementation :fire:

--- data/src/main/java/com/example/framework/database/PhotosGetDatabase.kt
+++ data/src/main/java/com/example/framework/database/PhotosGetDatabase.kt
@@ -4,11 +4,10 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.example.framework.datasource.GetDatabaseInterface
 import com.example.framework.models.PhotosEntityModel
 
 @Database(entities = [PhotosEntityModel::class], version = 1, exportSchema = false)
-abstract class PhotosGetDatabase : RoomDatabase(), GetDatabaseInterface {
+abstract class PhotosGetDatabase : RoomDatabase() {
 
     abstract fun daoInterface(): PhotosDataBaseDao
 
