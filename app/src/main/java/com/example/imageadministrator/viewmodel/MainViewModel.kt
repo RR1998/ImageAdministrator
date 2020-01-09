c324a157db5ae62a813fd7ae5581ce951604fdb5
Rodrigo Miranda <rodrigo.kreloco@gmail.com>
2020-01-09 13:57:11 -0600

    :art: Code cleanup MainViewModel

--- app/src/main/java/com/example/imageadministrator/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/imageadministrator/viewmodel/MainViewModel.kt
@@ -5,22 +5,14 @@ import androidx.lifecycle.ViewModel
 import com.example.core.domain.PhotosCleanModel
 import com.example.core.interactors.GetPhotos
 import com.example.imageadministrator.eventhandler.Event
-import kotlinx.coroutines.Job
 
 /**
  * MainViewModel its the class that calls the useCase to fill the view
  */
 
-class MainViewModel(var useCase: GetPhotos) : ViewModel() {
+class MainViewModel(private var useCase: GetPhotos) : ViewModel() {
 
     var itemClickEvent: MutableLiveData<Event<PhotosCleanModel>> = MutableLiveData()
-
-    private var mainViewModelJob = Job()
-
-    override fun onCleared() {
-        super.onCleared()
-        mainViewModelJob.cancel()
-    }
 
     fun getListPhoto(): MutableLiveData<List<PhotosCleanModel>> = useCase.invoke()
 }
