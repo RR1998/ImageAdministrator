3c2f8b35ac50279f4123c5a8c94d6efbbab675d0
RR <Roberto_Rubio98@outlook.com>
2020-01-13 10:38:14 -0600

    Added room implementation :heavy_plus_sign:

--- core/src/main/java/com/example/core/interactors/GetPhotos.kt
+++ core/src/main/java/com/example/core/interactors/GetPhotos.kt
@@ -6,7 +6,7 @@ import com.example.core.repository.PhotosDataSourceRepository
  * GetPhotos class is the class that is invoked in the MainViewModel and returns it
  * a List to fill the MainActivity view
  */
-
 class GetPhotos(private val photosRepository: PhotosDataSourceRepository) {
+
     operator fun invoke() = photosRepository.getPhotoData()
 }
