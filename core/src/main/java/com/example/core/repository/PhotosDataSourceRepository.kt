30b1e30f94c84d85855977c49097d13fa37581cc
RR <Roberto_Rubio98@outlook.com>
2020-01-13 11:12:56 -0600

    Refactored code :recycle:

--- core/src/main/java/com/example/core/repository/PhotosDataSourceRepository.kt
+++ core/src/main/java/com/example/core/repository/PhotosDataSourceRepository.kt
@@ -7,7 +7,7 @@ import com.example.core.domain.PhotosCleanModel
  * PhotosDataSourceRepository it's an interface that create a getPhotoData function that return
  * a MutableLiveDataList
  */
-
 interface PhotosDataSourceRepository {
+
     fun getPhotoData(): MutableLiveData<List<PhotosCleanModel>>
 }
