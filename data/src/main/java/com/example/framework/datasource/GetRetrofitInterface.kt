056f716e3647bfcf055d173ef7a61f074ae51b50
RR <Roberto_Rubio98@outlook.com>
2020-01-13 11:14:41 -0600

    Refactored code :recycle:

--- data/src/main/java/com/example/framework/datasource/GetRetrofitInterface.kt
+++ data/src/main/java/com/example/framework/datasource/GetRetrofitInterface.kt
@@ -6,7 +6,7 @@ import com.example.framework.remote.GetDataService
  * GetRetrofitInterface it's and interface that creates the getRetroFitInstances that returns
  * a retrofit instance
  */
-
 interface GetRetrofitInterface {
+
     fun getRetrofitInstance(): GetDataService
 }
