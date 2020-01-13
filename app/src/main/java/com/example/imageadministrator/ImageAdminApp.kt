2d6d05b7c969a6aec9b507a518a6c7ee77ac36c4
RR <Roberto_Rubio98@outlook.com>
2020-01-13 11:22:36 -0600

    Added startKoin method :sparkles:

--- app/src/main/java/com/example/imageadministrator/ImageAdminApp.kt
+++ app/src/main/java/com/example/imageadministrator/ImageAdminApp.kt
@@ -1,24 +1,27 @@
 package com.example.imageadministrator
 
 import android.app.Application
-import com.example.core.interactors.GetPhotos
-import com.example.framework.database.PhotosGetDatabase
-import com.example.framework.datasource.ModelSourceRepositoryImplementation
-import com.example.framework.remote.PhotosApi
+import com.example.imageadministrator.di.appModule
+import com.example.imageadministrator.di.coreModule
+import com.example.imageadministrator.di.dataModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidFileProperties
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
 
 /**
  * InitialValues is where the database and api are instantiated and can be used from other activities
  */
 class ImageAdminApp : Application() {
 
-    lateinit var database: PhotosGetDatabase
-    lateinit var useCase: GetPhotos
-
     override fun onCreate() {
+
         super.onCreate()
-        database = PhotosGetDatabase.getInstance(applicationContext)
-        val api = PhotosApi
-        val repository = ModelSourceRepositoryImplementation(api, database)
-        useCase = GetPhotos(repository)
+        startKoin {
+            androidLogger()
+            androidContext(this@ImageAdminApp)
+            androidFileProperties()
+            modules(listOf(appModule, dataModule, coreModule))
+        }
     }
 }
